8d1982ae7b35e37af4176b8f7013fded299fd3b7
Jose Diaz de Grenu <Jose.DiazdeGrenu@digi.com>
2019-04-13 16:03:31 +0200

    main: parse one Match of each Sport
    This allows to test the parse logic.
    
    Signed-off-by: Jose Diaz de Grenu <Jose.DiazdeGrenu@digi.com>

--- src/main/Main.java
+++ src/main/Main.java
@@ -2,22 +2,49 @@ package main;
 
 import java.util.List;
 
+import model.League;
+import model.Match;
 import model.ScrapException;
+import model.Sport;
 import scrapper.OddsPortalScrapper;
+import scrapper.ParserListener;
 import util.EclipseTools;
 
 public class Main {
 
 	public static void main(String[] args) throws Exception {
-		boolean pauseOnExit = false;
-
 		EclipseTools.fixConsole();
-		
+
 		System.setProperty("webdriver.chrome.driver", "C:\\chromedriver.exe");
 		try (OddsPortalScrapper scrapper = new OddsPortalScrapper()) {
+			boolean pauseOnExit = false;
 			/* Introduce another try block so that scrapper 
 			 * is not closed before the catch or finally blocks  */
 			try {
+				ParserListener listener = new ParserListener() {
+
+					@Override
+					public boolean onElementParsed(Match match) {
+						scrapper.parse(match);
+						return false;
+					}
+					
+					@Override
+					public boolean onElementParsed(League league) {
+						scrapper.parse(league);
+						return false;
+					}
+					
+					@Override
+					public boolean onElementParsed(Sport sport) {
+						scrapper.parse(sport);
+						return true;
+					}
+				};
+				
+				scrapper.registerListener(listener); 
+				scrapper.findSports();
+				
 				List<ScrapException> errors = scrapper.getErrors();
 				if (!errors.isEmpty()) {
 					System.err.println(errors.size() + " non-critical errors");
