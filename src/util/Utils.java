25760a07d2b91974443b6ec857739c1a6a740cea
Jose Diaz de Grenu <Jose.DiazdeGrenu@digi.com>
2019-04-18 19:41:36 +0200

    Utils: add method to convert tiestamp to LocalDateTime
    Useful when printing timestamps.
    
    Signed-off-by: Jose Diaz de Grenu <Jose.DiazdeGrenu@digi.com>

--- src/util/Utils.java
+++ src/util/Utils.java
@@ -1,10 +1,18 @@
 package util;
 
+import java.time.Instant;
+import java.time.LocalDateTime;
 import java.util.Collection;
+import java.util.TimeZone;
 
 import org.jsoup.nodes.Element;
 
 public class Utils {
+	public static LocalDateTime msTimestampToDate(long timestamp) {
+		return LocalDateTime.ofInstant(Instant.ofEpochMilli(timestamp), 
+		                                TimeZone.getDefault().toZoneId());
+	}
+	
 	public static double parseDoubleEmptyIsZero(String s) throws NumberFormatException {
 		if (s.trim().isEmpty())
 			return 0d;
