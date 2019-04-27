c02c1e8312ff74a2812dc3b5a5aa2904ba601f31
Jose Diaz de Grenu <Jose.DiazdeGrenu@digi.com>
2019-04-27 22:01:26 +0200

    Main: large rework, parse all matches and store them all
    This takes too much time (about 3 days) so I will probably
    move to parse only live matches for the moment.
    
    Signed-off-by: Jose Diaz de Grenu <Jose.DiazdeGrenu@digi.com>

--- src/main/Main.java
+++ src/main/Main.java
@@ -3,6 +3,7 @@ package main;
 import java.io.File;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.SQLException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
@@ -13,6 +14,7 @@ import model.Match;
 import model.MatchData;
 import model.ScrapException;
 import model.Sport;
+import persistence.SQLiteManager;
 import scrapper.OddsPortalScrapper;
 import scrapper.ParserListener;
 import util.EclipseTools;
@@ -20,96 +22,92 @@ import util.EclipseTools;
 public class Main {
 
 	private static final String ERROR_REPORT_PATH = "log";
+		
+	private static void parseMatches(SQLiteManager ddbbManager, OddsPortalScrapper scrapper) {
+		final String runStartDate = new SimpleDateFormat("yyyy-MM-dd_HH-mm-ss").format(new Date());
+		final List<Match> matches = new ArrayList<>();
+		
+		final ParserListener listener = new ParserListener() {
+
+			int nErrors = 0;
+			
+			@Override
+			public void onError(ScrapException error) {
+				nErrors++;
+
+				System.err.println("Error logged: " + error.getMessage());
+				String reportName = String.format("%s_%d.log", runStartDate, nErrors);
+				try (PrintWriter writer = new PrintWriter(new File(ERROR_REPORT_PATH, reportName))) {
+						error.logTo(writer);
+				} catch (IOException e) {
+					System.err.println("Could not log exception.");
+					e.printStackTrace();
+				}
+			}
+			
+			@Override
+			public boolean onElementParsed(Match match) {
+				matches.add(match);
+				return true;
+			}
+			
+			@Override
+			public boolean onElementParsed(League league) {
+				scrapper.parse(league);
+				return true;
+			}
+			
+			@Override
+			public boolean onElementParsed(Sport sport) {
+				scrapper.parse(sport);
+				return true;
+			}
+
+			@Override
+			public boolean onElementParsed(MatchData data) {
+				try {
+					ddbbManager.store(data);
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+
+				return true;
+			}
+		};
+		
+		scrapper.registerListener(listener);
+
+		long timeStart = System.currentTimeMillis();
+
+		try {
+			scrapper.findSports();
+		} catch (ScrapException e) {
+			e.printStackTrace();
+		}
+		
+		long timeEnd = System.currentTimeMillis();
+		
+		System.out.format("It took %d ms to parse %d matches (%.2f matches/sec)\n", timeEnd - timeStart, matches.size(), matches.size() / (double) ((timeEnd- timeStart)) / 1000);
+		
+		int i = 0;
+		timeStart = System.currentTimeMillis();
+		for (Match match : matches) {
+			System.out.println(i++ + "/" + matches.size());
+			scrapper.parse(match);
+		}
+		timeEnd = System.currentTimeMillis();
+		System.out.format("It took %d ms to load %d matches (%.2f sec/match)\n", timeEnd - timeStart, matches.size(), (timeEnd- timeStart) / (1000d * (double) matches.size()));
+	}
 	
 	public static void main(String[] args) throws Exception {
-		final String runDateString = new SimpleDateFormat("yyyy-MM-dd_HH-mm-ss").format(new Date());
 		EclipseTools.fixConsole();
 		System.setProperty("webdriver.chrome.driver", "C:\\chromedriver.exe");
-		
-		try (OddsPortalScrapper scrapper = new OddsPortalScrapper()) {
-			boolean pauseOnExit = false;
-			/* Introduce another try block so that scrapper 
-			 * is not closed before the catch or finally blocks  */
-			try {
-				List<Match> matches = new ArrayList<>();
-				ParserListener listener = new ParserListener() {
 
-					int nErrors = 0;
-					
-					@Override
-					public void onError(ScrapException error) {
-						nErrors++;
-
-						System.err.println("Error logged: " + error.getMessage());
-						String reportName = String.format("%s_%d.log", runDateString, nErrors);
-						try (PrintWriter writer = new PrintWriter(new File(ERROR_REPORT_PATH, reportName))) {
-								error.logTo(writer);
-						} catch (IOException e) {
-							System.err.println("Could not log exception.");
-							e.printStackTrace();
-						}
-						
-						try {
-							System.in.read();
-						} catch (IOException e) {
-							e.printStackTrace();
-						}
-					}
-					
-					@Override
-					public boolean onElementParsed(Match match) {
-						matches.add(match);
-						return true;
-					}
-					
-					@Override
-					public boolean onElementParsed(League league) {
-						scrapper.parse(league);
-						return true;
-					}
-					
-					@Override
-					public boolean onElementParsed(Sport sport) {
-						scrapper.parse(sport);
-						return true;
-					}
-
-					@Override
-					public boolean onElementParsed(MatchData data) {
-						System.out.println(data.match + " parsed!");
-						return true;
-					}
-				};
-				
-				scrapper.registerListener(listener);
-				long timeStart = System.currentTimeMillis();
-				scrapper.findSports();
-				long timeEnd = System.currentTimeMillis();
-				
-				System.out.format("It took %d ms to parse %d matches (%.2f matches/sec)", timeEnd- timeStart, matches.size(), matches.size() / (double) ((timeEnd- timeStart)) / 1000);
-				
-				timeStart = System.currentTimeMillis();
-				int i = 0;
-				for (Match match : matches) {
-					System.out.println(i++ + "/" + matches.size());
-					scrapper.parse(match);
-				}
-				timeEnd = System.currentTimeMillis();
-				System.out.format("It took %d ms to load %d matches (%.2f sec/match)", timeEnd - timeStart, matches.size(), (timeEnd- timeStart) / (1000d * (double) matches.size()));
-				
-				
-			} catch (Exception e) {
-				System.err.println("Critical exception: ");
-				e.printStackTrace();
-				
-				pauseOnExit = true;
-			} finally {
-				if (pauseOnExit) {
-					System.err.flush();
-		
-					System.out.println("Press any key to close.");
-					System.in.read();
-				}
+		try (SQLiteManager ddbbManager = new SQLiteManager()) {
+			ddbbManager.ensureDDBBCreated();
+			
+			try (OddsPortalScrapper scrapper = new OddsPortalScrapper()) {
+				parseMatches(ddbbManager, scrapper);
 			}
 		}
 	}
