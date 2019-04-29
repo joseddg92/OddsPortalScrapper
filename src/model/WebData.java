2b17330e2efe610687074bc33b40ba8c158554e3
Jose Diaz de Grenu <Jose.DiazdeGrenu@digi.com>
2019-04-29 18:35:08 +0200

    WebData: implement lazy parsing
    Parse the document only when asked for it. This means that (at least most
    of the time) the parsing will be done by a parsing thread, not a scrapping
    one (which are always more busy).
    
    Signed-off-by: Jose Diaz de Grenu <Jose.DiazdeGrenu@digi.com>

--- src/model/WebData.java
+++ src/model/WebData.java
@@ -7,18 +7,34 @@ import org.openqa.selenium.WebDriver;
 public class WebData {
 
 	private Document doc;
+	private String pageSource;
+	private String pageUrl;
+	
 	
 	private WebData() {
 		
 	}
 	
 	public Document getDoc() {
+		ensureDocParsed();
+
 		return doc;
 	}
 	
 	public static WebData fromDriver(WebDriver driver) {
 		final WebData wb = new WebData();
-		wb.doc = Jsoup.parse(driver.getPageSource(), driver.getCurrentUrl());
+		wb.pageSource = driver.getPageSource();
+		wb.pageUrl = driver.getCurrentUrl();
+
 		return wb;
+	}
+	
+	private void ensureDocParsed() {
+		if (doc != null)
+			return;
+	
+		doc = Jsoup.parse(pageSource, pageUrl);
+		pageSource = null;
+		pageUrl = null;
 	}
 }
