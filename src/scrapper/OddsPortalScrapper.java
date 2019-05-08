8aeddeaabd20133b020e0a1ca0ffd4be77f72c93
Jose Diaz de Grenu <Jose.DiazdeGrenu@digi.com>
2019-05-08 23:26:32 +0200

    OddsPortalScrapper: reimplement parse(Match)
    Use the new getAndHandle generic method and get rid of the specific
    SeleniumChromeProvider::getAllTabs().
    
    Signed-off-by: Jose Diaz de Grenu <Jose.DiazdeGrenu@digi.com>

--- src/scrapper/OddsPortalScrapper.java
+++ src/scrapper/OddsPortalScrapper.java
@@ -1,9 +1,9 @@
 package scrapper;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Map.Entry;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
@@ -14,9 +14,17 @@ import org.jsoup.nodes.Element;
 import org.jsoup.nodes.Node;
 import org.jsoup.nodes.TextNode;
 import org.jsoup.select.Elements;
+import org.openqa.selenium.By;
 import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.TimeoutException;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import htmlProvider.SeleniumChromeProvider;
+import htmlProvider.WebDriverHandler;
 import model.Country;
 import model.League;
 import model.Match;
@@ -27,7 +35,6 @@ import model.ScrapException;
 import model.Sport;
 import model.WebData;
 import model.WebSection;
-import util.Pair;
 import util.StringDate;
 import util.Utils;
 
@@ -215,151 +222,226 @@ public class OddsPortalScrapper implements AutoCloseable {
 		
 		return map;
 	}
-	
+
+	private static void waitLoadSpinner(WebDriverHandler wdh) {
+		/* 
+		 * Wait for the spinner to appear, and then to disappear. 
+		 * If it is not found within a second, keep going as maybe
+		 * we didn't give it time to appear.
+		 */
+		try {
+			new WebDriverWait(wdh.getDriver(), 1, 50).until(ExpectedConditions.visibilityOfElementLocated(By.id("event-wait-msg-main")));
+		} catch (TimeoutException e) {}
+		
+		try {
+			new WebDriverWait(wdh.getDriver(), 60, 50).until(ExpectedConditions.invisibilityOfElementLocated(By.id("event-wait-msg-main")));
+		} catch (TimeoutException e) {}
+		
+		/* After the spinner has disappeared, js is changing the DOM, so wait for it to finish */
+		wdh.waitJs();
+	}
+		
 	public void parse(Match m) {
-		Map<WebSection, Pair<WebData, List<String>>> tabs; 
-		try {
-			tabs = htmlProvider.getAllTabs(m.url);
-		} catch (ScrapException e) {
-			logError(e);
-			return;
-		}
-		
-		if (tabs.isEmpty()) {
-			logError(new ScrapException("No tabs! " + m));
-			return;
-		}
-
-		final WebData firstWebData = tabs.values().iterator().next().first;
-		final Document firstTab = firstWebData.getDoc();
-		Element dateElement = firstTab.selectFirst("p.date");
-		if (dateElement == null) {
-			logError(new ScrapException("Could not find match time! " + m, firstWebData));
-			return;
-		}
-
-		final Pattern dateClassPattern = Pattern.compile("t([0-9]+)-");
-		Matcher matcher = dateClassPattern.matcher(dateElement.attr("class"));
-		if (!matcher.find() || matcher.groupCount() > 1) {
-			logError(new ScrapException("Could not parse time: " + dateElement.attr("class") + "," + m, firstWebData, firstTab));
-			return;
-		}
-		long dateTimestamp;
-		try {
-			dateTimestamp = Long.parseLong(matcher.group(1));
-		} catch (NumberFormatException e) {
-			logError(new ScrapException("Could not parse time: " + dateElement.attr("class") + "," + m, firstWebData, firstTab, e));
-			return;
-		}
-		
-		final MatchData matchData = new MatchData(m, dateTimestamp);
-		
-		for (Entry<WebSection, Pair<WebData, List<String>>> sectionEntry : tabs.entrySet()) {
-			final WebSection section = sectionEntry.getKey();
-			final WebData webData = sectionEntry.getValue().first;
-			final Document doc = webData.getDoc();
-			final List<Element> htmlOddHistoryFragmentsList = 
-					sectionEntry.getValue().second.stream().map(
-							s -> s == null ? null : Jsoup.parse(s)).collect(Collectors.toList()
-					);
-				
-			/* Do not parse exchanges for now */
-			final Elements oddTables = doc.select("div#odds-data-table div.table-container:not(.exchangeContainer)");
-			if (oddTables.isEmpty()) {
-				logError(new ScrapException("Could not locate any oddTable, " + section +", " + m, webData, doc));
-				return;
+		MatchData data = htmlProvider.getAndHandle(m.url, (webData, driverHandler) -> {
+			final RemoteWebDriver driver = driverHandler.getDriver();
+			Document doc = webData.getDoc();
+			Element dateElement = doc.selectFirst("p.date");
+			if (dateElement == null) {
+				logError(new ScrapException("Could not find match time! " + m, webData));
+				return null;
 			}
 
-			for (Element oddTable : oddTables) {
-				String oddTableTitle = null;
-				if (oddTables.size() > 1) {
-					oddTableTitle = oddTable.selectFirst("strong").text();
-				}
-				
-				Element headerRow = oddTable.selectFirst("thead > tr");
-				if (headerRow == null) {
-					logError(new ScrapException("No header row while parsing " + section + "in " + m, webData, doc));
-					continue;
-				}
-				
-				List<String> columns = headerRow.select("tr a").stream().map(e -> e.text()).collect(Collectors.toList());
-				
-				/* Check first and last columns are as expected, and remove them */
-				String firstColumn = columns.get(0);
-				String lastColumn = columns.get(columns.size() - 1);
-				if (!firstColumn.trim().equals("Bookmakers")) {
-					logError(new ScrapException("Strange first column: " + columns.get(0), webData, headerRow));
-					continue;
-				} else {
-					columns.remove(firstColumn);
-				}
-				if (lastColumn.trim().equals("Payout")) {
-					columns.remove(lastColumn);
-				}
-
-				if (columns.isEmpty())
-					logError(new ScrapException("There are no columns!", webData, oddTable));
-				
-				Elements rows = oddTable.select("table > tbody > tr");
-				if (rows.isEmpty())
-					logError(new ScrapException("There are no rows!", webData, oddTable));
-
-				for (Element row : rows) {
-					Elements oddElements = row.select("td.odds");
-					
-					if (oddElements.isEmpty())
-						continue;
-					
-					/* For now assume first column to be the bethouse */
-					Element bethouseElement = row.child(0);
-					String betHouse = Utils.combineAllText(bethouseElement.select("a"));
-					
-					if (oddElements.size() != columns.size()) {
-						logError(new ScrapException("Strange number of odds, expected: " + columns + " only got: " + oddElements.size(), webData, row));
-						continue;
-					}
-	
-					for (int i = 0; i < oddElements.size(); i++) {
-						Element oddElement = oddElements.get(i);
-						String column = columns.get(i);
-						/* Some times there's a nested <a> element with a link to the bethouse */
-						Element aElement = oddElement.selectFirst("a");
-						oddElement = aElement != null ? aElement : oddElement;
-						double odd = 0;
-						try {
-							odd = Utils.parseDoubleEmptyIsZero(oddElement.text());
-						} catch (NumberFormatException e) {
-							logError(new ScrapException(m + ", " + section + " strange odd cell does not have an odd", webData, row, e));
-						}
-						
-						final OddKey key = new OddKey(section, oddTableTitle, betHouse, column);
-						Map<StringDate, Double> oddsForKey;
-						Element fragment = null;
-						try {
-							fragment = htmlOddHistoryFragmentsList.remove(0);
-							oddsForKey = parseOddHistory(fragment);
-						} catch (IndexOutOfBoundsException e) {
-							logError(new ScrapException("htmlOddHistoryFragments was short (current element " + i + " )", webData, doc));
-							/* At least used the parsed odd with current timestamp */
-							oddsForKey = new LinkedHashMap<>(1);
-							oddsForKey.put(new StringDate(System.currentTimeMillis()), odd);
-						}
-						if (oddsForKey.isEmpty())
-							logError(new ScrapException("Empty oddKey: " + key, webData, oddElement));
-						else 
-							matchData.addOdds(key, oddsForKey);
-					}
-				}
+			final Pattern dateClassPattern = Pattern.compile("t([0-9]+)-");
+			Matcher matcher = dateClassPattern.matcher(dateElement.attr("class"));
+			if (!matcher.find() || matcher.groupCount() > 1) {
+				logError(new ScrapException("Could not parse time: " + dateElement.attr("class") + "," + m, webData, doc));
+				return null;
+			}
+			long dateTimestamp;
+			try {
+				dateTimestamp = Long.parseLong(matcher.group(1));
+			} catch (NumberFormatException e) {
+				logError(new ScrapException("Could not parse time: " + dateElement.attr("class") + "," + m, webData, doc, e));
+				return null;
+			}
+			final MatchData matchData = new MatchData(m, dateTimestamp);
+			
+			Elements tabs = webData.getDoc().select("div#bettype-tabs li a");
+			/* The tab that is selected by default is the current element and does not have
+			 * a <a> element, so manually append it. */
+			Element activeTab = webData.getDoc().selectFirst("div#bettype-tabs li.active strong span");
+			if (activeTab == null) {
+				if (tabs.size() != 0)
+					logError(new ScrapException("tabs != 0 but no active tab", webData));
 			}
 			
-			if (!htmlOddHistoryFragmentsList.isEmpty())
-				logError(new ScrapException("Extra fragments...", webData, doc));
-		}
+			String tabTitle = activeTab.text();
+			tabs.add(0, activeTab);
+			boolean firstTab = true;
+			for (Element tab : tabs) {
+				// Skip "More bets" tab
+				if (tab.parent().attr("class").contains("more"))
+					continue;
+
+				if (!firstTab) {
+					tabTitle = tab.attr("title");
+					if (tabTitle.isEmpty())
+						tabTitle = tab.text();
+
+					String jsCode = tab.attr("onmousedown");
+					driverHandler.executeScript(jsCode);
+					waitLoadSpinner(driverHandler);
+					webData = driverHandler.get();
+				} else {
+					firstTab = false;
+				}
+				
+				Elements subtabs = webData.getDoc().select("div#bettype-tabs-scope ul[style=\"display: block;\"] li a");
+				Element activeSubtab = webData.getDoc().selectFirst("div#bettype-tabs-scope ul[style=\"display: block;\"] li.active strong span");
+				if (activeSubtab == null)
+					continue;
+				String subtabTitle = activeSubtab.text();
+				subtabs.add(0, activeSubtab);
+				boolean firstSubTab = true;
+				for (Element subtab : subtabs) {
+					if (!firstSubTab) {
+						subtabTitle = subtab.attr("title");
+						String jsCode = subtab.attr("onmousedown");
+						driverHandler.executeScript(jsCode);
+						waitLoadSpinner(driverHandler);
+						webData = driverHandler.get();
+					} else {
+						firstSubTab = false;
+					}
+					
+					/* 
+					 * Expand all bet groups by 'clicking' on them. Sometimes the rows are
+					 * already expanded, so expand only if they don' contain data to be
+					 * parsed (table.table-main)
+					 */
+					int rowsToBeExpanded = 0;
+					int rowsExpanded = 0;
+					for (Element rowToBeExpanded : webData.getDoc().select("#odds-data-table > div.table-container")) {
+						rowsToBeExpanded++;
+						final Element jsCodeContainer = rowToBeExpanded.selectFirst("div > strong > a");
+						final boolean needsToBeExpanded = rowToBeExpanded.selectFirst("table.table-main") == null;
+						if (needsToBeExpanded && jsCodeContainer != null) {
+							rowsExpanded++;
+							driverHandler.executeScript(jsCodeContainer.attr("onclick"));
+						}
+					}
+
+					final WebSection section = new WebSection(tabTitle, subtabTitle);
+					webData = driverHandler.get();
+					doc = webData.getDoc();
+
+					final Elements oddTables = doc.select("div#odds-data-table div.table-container:not(.exchangeContainer)");
+					if (oddTables.isEmpty()) {
+						final String errorMsg = String.format("Could not locate any oddTable tbe: %d, e: %d. %s, %s", 
+														 rowsToBeExpanded, rowsExpanded, section, m);
+						logError(new ScrapException(errorMsg, webData, doc));
+						return null;
+					}
+					
+					for (Element oddTable : oddTables) {
+						String oddTableTitle = null;
+						if (oddTables.size() > 1) {
+							oddTableTitle = oddTable.selectFirst("strong").text();
+						}
+						
+						Element headerRow = oddTable.selectFirst("thead > tr");
+						if (headerRow == null) {
+							logError(new ScrapException("No header row while parsing " + section + "in " + m, webData, doc));
+							continue;
+						}
+						
+						List<String> columns = headerRow.select("tr a").stream().map(e -> e.text()).collect(Collectors.toList());
+						
+						/* Check first and last columns are as expected, and remove them */
+						String firstColumn = columns.get(0);
+						String lastColumn = columns.get(columns.size() - 1);
+						if (!firstColumn.trim().equals("Bookmakers")) {
+							logError(new ScrapException("Strange first column: " + columns.get(0), webData, headerRow));
+							continue;
+						} else {
+							columns.remove(firstColumn);
+						}
+						if (lastColumn.trim().equals("Payout"))
+							columns.remove(lastColumn);
+
+						if (columns.isEmpty())
+							logError(new ScrapException("There are no columns!", webData, oddTable));
+						
+						Elements rows = oddTable.select("table > tbody > tr");
+						if (rows.isEmpty())
+							logError(new ScrapException("There are no rows!", webData, oddTable));
+
+						
+						for (Element row : rows) {
+							String rowSelector = row.cssSelector();
+							Elements oddElements = row.select("td.odds");
+							
+							if (oddElements.isEmpty())
+								continue;
+							
+							if (oddElements.size() != columns.size()) {
+								logError(new ScrapException("Strange number of odds, expected: " + columns + " only got: " + oddElements.size(), webData, row));
+								continue;
+							}
+
+							/* For now assume first column to be the bethouse */
+							Element bethouseElement = row.child(0);
+							String betHouse = Utils.combineAllText(bethouseElement.select("a"));
+
+							for (int i = 0; i < oddElements.size(); i++) {
+								final String column = columns.get(i);
+								final OddKey key = new OddKey(section, oddTableTitle, betHouse, column);
+								
+								Element oddElement = oddElements.get(i);
+								Element aElement = oddElement.selectFirst("a");
+								String oddSelector = String.format("%s > td.odds:nth-child(%d)", rowSelector, oddElement.elementSiblingIndex() + 1, aElement);
+								
+								/* Some times there's a nested <a> element with a link to the bethouse */
+								if (aElement != null) {
+									oddElement = aElement;
+									oddSelector += " > a";
+								}
+								
+								Map<StringDate, Double> oddsForKey = null;
+								Element fragment = null;
+								try {
+									final WebElement elementThatShowsHistoric = driver.findElement(By.cssSelector(oddSelector));
+									new Actions(driver).moveToElement(elementThatShowsHistoric).perform();
+									fragment = Jsoup.parse(driver.findElementById("tooltipdiv").getAttribute("outerHTML"));
+									oddsForKey = parseOddHistory(fragment);
+								} catch (NoSuchElementException e) {
+									logError(new ScrapException("Could not get odd history element", webData, doc, e));
+									try {
+										/* At least used the parsed odd with current timestamp */
+										double odd = Utils.parseDoubleEmptyIsZero(oddElement.text());
+										oddsForKey = Collections.singletonMap(new StringDate(System.currentTimeMillis()), odd);
+									} catch (NumberFormatException e2) {
+										logError(new ScrapException(m + ", " + section + " strange odd cell does not have an odd", webData, row, e2));
+									}
+								}
+
+								if (oddsForKey == null || oddsForKey.isEmpty())
+									logError(new ScrapException("Empty oddKey: " + key + ", tbe: " + rowsToBeExpanded + ", e: " + rowsExpanded, webData, fragment));
+								else 
+									matchData.addOdds(key, oddsForKey);
+							}
+						}
+					}
+				}
+			}
+
+			return matchData;
+		});
 		
-		if (matchData.getOdds().isEmpty())
-			logError(new ScrapException("Empty matchData D: " + matchData));
+		if (data == null || data.getOdds().isEmpty())
+			logError(new ScrapException("Empty matchData D: " + data));
 		else
-			fireEventCheckStop(matchData);
+			fireEventCheckStop(data);
 	}
 	
 	@Override
